13525582fdef5d4e8b5fca4d3e8d3ff1a7f85862
蔡浩浩 <caihh@missfresh.cn>
2019-01-06 20:36:29 +0800

    commit

---
+++ Test/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Test
+//
+//  Created by karl on 2019/01/06.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    NSObject *o = [[NSObject alloc] init];
+    return 0;
+}
---
+++ objc-debug/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  objc-debug
+//
+//  Created by quan on 2018/12/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    NSObject *obj = [NSObject new];
+    return 0;
+}
